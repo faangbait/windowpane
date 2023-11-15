7967c55ca10b0b111cc157a53603704357557026
S Smith <root@madeof.glass>
2023-11-15 06:17:16 +0000

    Application

---
+++ github.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"html/template"
+	"log"
+	"strings"
+
+	"github.com/google/go-github/v56/github"
+)
+
+const GithubUsername = "faangbait"
+
+func getClient() *github.Client {
+	client := github.NewClient(nil)
+	return client
+}
+
+func getBasicInfo(c *github.Client) (ghUser, *github.Response) {
+	u, resp, err := c.Users.Get(context.TODO(), GithubUsername)
+	if err != nil {
+		log.Println(err.Error())
+		return ghUser{}, resp
+	}
+
+	return ghUser{
+		Name:      u.GetLogin(),
+		URL:       u.GetHTMLURL(),
+		AvatarURL: u.GetAvatarURL(),
+	}, resp
+}
+
+func getRepositories(c *github.Client) ([]ghRepository, *github.Response) {
+	var repoList []ghRepository
+
+	repos, resp, err := c.Repositories.List(context.TODO(), GithubUsername, nil)
+	if err != nil {
+		log.Println(err.Error())
+		return repoList, resp
+	}
+
+	for _, r := range repos {
+		repoList = append(repoList, ghRepository{
+			Owner: ghUser{
+				Name:      r.Owner.GetLogin(),
+				URL:       r.Owner.GetHTMLURL(),
+				AvatarURL: r.Owner.GetAvatarURL(),
+			},
+			Name:        r.GetName(),
+			Language:    r.GetLanguage(),
+			Description: r.GetDescription(),
+			URL:         r.GetHTMLURL(),
+			Fork:        r.GetFork(),
+			Archived:    r.GetArchived(),
+		})
+	}
+
+	return repoList, resp
+}
+
+func getEvents(c *github.Client) ([]ghEvent, *github.Response) {
+	var eventList []ghEvent
+	opts := github.ListOptions{
+		Page:    1,
+		PerPage: 100,
+	}
+
+	events, resp, err := c.Activity.ListEventsPerformedByUser(context.TODO(), GithubUsername, false, &opts)
+	if err != nil {
+		log.Println(err.Error())
+		return eventList, resp
+	}
+
+	for _, e := range events {
+		var pushEvent github.PushEvent
+		var commits []ghCommit
+		pushPayload := ghPush{}
+
+		err := json.Unmarshal(e.GetRawPayload(), &pushEvent)
+
+		if err == nil {
+			for _, hc := range pushEvent.GetCommits() {
+				commits = append(commits, ghCommit{
+					Message: template.HTML(strings.Replace(hc.GetMessage(), "\n", "<br />", -1)),
+					SHA:     hc.GetSHA(),
+				})
+			}
+
+			pushPayload = ghPush{
+				Head:    pushEvent.GetHead(),
+				Commits: commits,
+			}
+		}
+
+		repoName, _ := strings.CutPrefix(e.Repo.GetName(), "faangbait/")
+
+		eventList = append(eventList, ghEvent{
+			Actor: ghUser{
+				Name:      e.Actor.GetLogin(),
+				URL:       e.Actor.GetHTMLURL(),
+				AvatarURL: e.Actor.GetAvatarURL(),
+			},
+			Repo: ghRepository{
+				Owner: ghUser{
+					Name:      e.Repo.Owner.GetLogin(),
+					URL:       e.Repo.Owner.GetHTMLURL(),
+					AvatarURL: e.Repo.Owner.GetAvatarURL(),
+				},
+				Name:        repoName,
+				Language:    e.Repo.GetLanguage(),
+				Description: e.Repo.GetDescription(),
+				URL:         e.Repo.GetHTMLURL(),
+				Fork:        e.Repo.GetFork(),
+				Archived:    e.Repo.GetArchived(),
+			},
+			Type:    e.GetType(),
+			Payload: pushPayload,
+		})
+	}
+
+	return eventList, resp
+}
+
+type ghData struct {
+	UserInfo ghUser
+	Repos    []ghRepository
+	Events   []ghEvent
+}
+
+type ghUser struct {
+	Name      string
+	URL       string
+	AvatarURL string
+}
+
+type ghRepository struct {
+	Owner       ghUser
+	Name        string
+	Language    string
+	Description string
+	URL         string
+	Fork        bool
+	Archived    bool
+}
+
+type ghEvent struct {
+	Actor   ghUser
+	Repo    ghRepository
+	Type    string
+	Payload ghPush
+}
+
+type ghCommit struct {
+	Message template.HTML
+	SHA     string
+}
+
+type ghPush struct {
+	Head    string
+	Commits []ghCommit
+}
--- main.go
+++ main.go
@@ -1,3 +1,76 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("files/"))
+
+	mux.Handle("/files/", http.StripPrefix("/files", fileServer))
+	mux.HandleFunc("/", getTemplate)
+
+	checkError(http.ListenAndServe(":3333", mux))
+}
+
+func checkError(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func getTemplate(w http.ResponseWriter, r *http.Request) {
+	layout := filepath.Join("templates", "layout.html")
+
+	c := getClient()
+	user, resp := getBasicInfo(c)
+	if resp.StatusCode == http.StatusForbidden {
+		http.Error(w, "403 Forbidden (likely rate limited)", http.StatusForbidden)
+		return
+	} else if resp.StatusCode != 200 {
+		log.Println(resp.Body)
+		http.Error(w, http.StatusText(resp.StatusCode), resp.StatusCode)
+		return
+	}
+
+	repos, resp := getRepositories(c)
+	if resp.StatusCode == http.StatusForbidden {
+		http.Error(w, "403 Forbidden (likely rate limited)", http.StatusForbidden)
+		return
+	} else if resp.StatusCode != 200 {
+		log.Println(resp.Body)
+		http.Error(w, http.StatusText(resp.StatusCode), resp.StatusCode)
+		return
+	}
+
+	events, resp := getEvents(c)
+	if resp.StatusCode == http.StatusForbidden {
+		http.Error(w, "403 Forbidden (likely rate limited)", http.StatusForbidden)
+		return
+	} else if resp.StatusCode != 200 {
+		log.Println(resp.Body)
+		http.Error(w, http.StatusText(resp.StatusCode), resp.StatusCode)
+		return
+	}
+
+	data := ghData{
+		UserInfo: user,
+		Repos:    repos,
+		Events:   events,
+	}
+
+	tmpl, err := template.ParseGlob(layout)
+	checkError(err)
+
+	err = tmpl.ExecuteTemplate(w, "layout", data)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
+}
